70244a4c9004637ddb102dbd274ebebe384d400f
Alex van Winkel <alexicoo@gmail.com>
2021-11-04 22:13:58 -0400

    added database, new.html and some of the front end

---
+++ db.py
@@ -0,0 +1,95 @@
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print("** SQLite DB active, version:",sqlite3.version)
+    except Error as e:
+        print("** ERROR **", e)
+    finally:
+        if conn:
+            conn.close()
+
+def connect_to_db(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print("** Connection established")
+        return conn
+    except Error as e:
+        print(e)
+
+    return conn
+
+def create_tables():
+    print(">> Checkpoint Beta" )
+    database = "knowledge.db"
+    conn = connect_to_db(database)
+
+    c = conn.cursor()
+
+    '''query = "drop table users"
+    c.execute(query)
+    query = "drop table domains"
+    c.execute(query)
+    '''
+
+    query = """ CREATE TABLE IF NOT EXISTS users (
+                                        user_id_id integer PRIMARY KEY,
+                                        name text NOT NULL,
+                                        username text NOT NULL,
+                                        password text NOT NULL,
+                                        role text NOT NULL
+                                    ); """
+    c.execute(query)
+    
+    query = """ CREATE TABLE IF NOT EXISTS domains (
+                                        domain_id integer PRIMARY KEY,
+                                        name text NOT NULL                                    
+                                    ); """
+    c.execute(query)
+    
+    query = """ CREATE TABLE IF NOT EXISTS topics (
+                                        topic_id integer PRIMARY KEY,
+                                        name text NOT NULL,
+                                        domain int,
+                                        foreign key(domain) references domains(domain_id)
+                                    ); """
+    c.execute(query)
+    
+    query = """ CREATE TABLE IF NOT EXISTS knowledge (
+                                        know_id integer PRIMARY KEY,
+                                        domain int,
+                                        topic int,
+                                        problem text NOT NUll,
+                                        solution text NOT NULL,
+                                        foreign key(domain) references domains(domain_id),
+                                        foreign key(domain) references domains(domain_id)
+                                    ); """
+    c.execute(query)
+
+    print("** Tables created")
+
+def create_starter_data():
+    database = "knowledge.db"
+    conn = connect_to_db(database)
+    c = conn.cursor()
+    query = '''INSERT INTO domains (name)
+                VALUES( "HTML"), ("CSS"), ("HTML/CSS"), ("JS"), ("HTML/CSS/JS"), ("JQuery"), ("Python"), ("Flask"), ("Misc");'''
+    c.execute(query)
+    conn.commit()
+
+def get_domains():
+    database = "knowledge.db"
+    conn = connect_to_db(database)
+    c = conn.cursor()
+    query = '''select name from domains'''
+    c.execute(query)
+    data = c.fetchall()
+    output = []
+    for d in data:
+        output.append(d[0])
+    return output
--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,28 @@
 from flask import Flask,redirect,url_for,render_template,request
+import sqlite3
+from sqlite3 import Error
+from db import create_connection, create_tables, create_starter_data, get_domains
 
 app=Flask(__name__, static_url_path='/static')
+
+create_connection(r"knowledge.db")
+create_tables()
+#create_starter_data()  # run just once to get the first domains in the db
+print(">> Checkpoint Alpha" )
+
+
 @app.route('/',methods=['GET','POST'])
 def home():
-    return render_template('index.html')
+    domain_list = []
+    domain_list = get_domains()
+    return render_template('index.html', domain_list=domain_list)
+
+@app.route('/new',methods=['GET','POST'])
+def new():
+    print("## New !")
+    domain_list = []
+    domain_list = get_domains()
+    return render_template('new.html', domain_list=domain_list)
 
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
